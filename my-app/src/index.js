9bdc2599b6782f290c10c05ed330222b82197b87
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-08-27 08:54:56 +0900

    クリックされるたびにxIsNextを反転する

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -35,7 +35,10 @@ class Board extends React.Component {
     handleClick(i) {
         const squares = this.state.squares.slice() // slice()は配列のコピーを生成
         squares[i] = 'X'
-        this.setState({squares: squares})
+        this.setState({
+            squares: squares,
+            xIsNext: !this.state.xIsNext,
+        })
     }
     render() {
       const status = 'Next player: X';
