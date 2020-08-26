7017a1d8517fd36fa6f6b03ccdc868fdc13b8fea
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-08-27 08:39:06 +0900

    BoardでSquareのbuttonのonClickイベントを受け取りたいので変更

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -14,9 +14,9 @@ class Square extends React.Component {
       return (
         <button 
             className="square" 
-            onClick={() => this.setState({value: 'X'})}
+            onClick={() => this.props.onClick()}
         >
-          {this.state.value}
+          {this.props.value}
         </button>
       );
     }
@@ -31,9 +31,17 @@ class Board extends React.Component {
     }
 
     renderSquare(i) {
-        return <Square  value={this.state.squares[i]} />;
+        return (
+            <Square  
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />
+        );
     }
   
+    handleClick(i) {
+        
+    }
     render() {
       const status = 'Next player: X';
   
