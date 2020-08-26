9b1951f063b12d81c8d186371baf15f355a1dbce
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-08-27 08:55:53 +0900

    xIsNextがtrueならXを、falseならOを出力するように変更

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -34,7 +34,7 @@ class Board extends React.Component {
   
     handleClick(i) {
         const squares = this.state.squares.slice() // slice()は配列のコピーを生成
-        squares[i] = 'X'
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
         this.setState({
             squares: squares,
             xIsNext: !this.state.xIsNext,
