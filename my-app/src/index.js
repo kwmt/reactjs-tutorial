512bf14d9eda0a3b398cd863a9dbf4b2c7f479b4
Yasutaka Kawamoto <kwmt27@gmail.com>
2020-08-27 09:47:44 +0900

    いま何番手かを示すstepNumberを定義

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -75,6 +75,7 @@ class Game extends React.Component {
             history: history.concat([{
                 squares: squares,
             }]),
+            stepNumber: 0,
             xIsNext: !this.state.xIsNext,
         })
     }
